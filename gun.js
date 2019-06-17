4c722cd3b9caf89ca62e80b100d70aa322e70bf3
nicsachin <nicsachin007@gmail.com>
2019-06-17 10:11:24 +0530

    done

--- gun.js
+++ gun.js
@@ -1,36 +1,38 @@
-let  a = [1,2,3,4,5,6,7,8,9,10]
 
+let n = 1000
+let a = []
+for (let i = 0; i < n; i++) {
+    a.push(i + 1)
+
+}
 let flag = false
 let kill = false
 let i = 0
-while(i<a.length)
-{ 
-    if(new Set(a).size == 2)
-     {
-         break
-     }
+while (i < a.length) {
+    if (new Set(a).size == 2) {
+        break
+    }
 
- if(a[i]!=0 && kill == true)
- {
-    a[i] =0 
-     kill = false
- }
- else
- {
-     if(a[i]!=0)
-      {
-           
-           kill = true
-           
-      }
- }
+    if (a[i] != 0 && kill) {
+        a[i] = 0
+        kill = false
+    }
+    else {
+        if (a[i] != 0) {
 
- if(i+1 == a.length) 
-   {    i=0}
-   else{
-       i++
-   }
+            kill = true
+
+        }
+    }
+
+    if (i + 1 == a.length) { i = 0 }
+    else {
+        i++
+    }
 
 }
 
-console.log('>>' , a)
+a.map((data) => {
+    if (data != 0)
+        console.log(data)
+})
